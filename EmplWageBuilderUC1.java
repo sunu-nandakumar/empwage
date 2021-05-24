db3b2050812f3b9b75b834ad2be792674f3f4689
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-05-24 21:06:55 +0530

    changing the code from if else to switch

--- EmplWageBuilderUC1.java
+++ EmplWageBuilderUC1.java
@@ -2,27 +2,30 @@ public class EmplWageBuilderUC1{
 
 public static void main(String args[]){
 	
-int is_FullTime=1;
-int is_partTime=2;
+
 int Wage_per_hr=20;
 int Daily_wage=0;
 
-double emplCheck = Math.floor(Math.random () * 10 ) % 3;
+int emplCheck = (int) (Math.floor(Math.random () * 10 ) % 3) ;
 
-	if(emplCheck == is_FullTime)
-	{
+	switch(emplCheck) 
+{
+	case  1:
+	
 	Daily_wage = Wage_per_hr * 8 ;
 	System.out.println(" the daily wage of employee is " + Daily_wage );
 	System.out.println("Employee is present");
+	break;
 
-	}
-	else if( emplCheck == is_partTime)
-	{ 
+	case 2:
+	
 	Daily_wage = Wage_per_hr * 4;
 	System.out.println(" the daily wage of employee is " + Daily_wage );
 	System.out.println("Employee is part timmer");
-	}
-	else{
+	break;
+
+	default:
+
 	System.out.println("employee is absent");
 	
 }
