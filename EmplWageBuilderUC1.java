5fcb4a29eb12ec47bdd70c31d74e6965ab089d6f
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-05-24 19:56:23 +0530

    adding parttime module to the branch

--- EmplWageBuilderUC1.java
+++ EmplWageBuilderUC1.java
@@ -3,19 +3,29 @@ public class EmplWageBuilderUC1{
 public static void main(String args[]){
 	
 int is_FullTime=1;
+int is_partTime=2;
 int Wage_per_hr=20;
 int Daily_wage=0;
-double emplCheck = Math.floor(Math.random () * 10 ) % 2;
+
+double emplCheck = Math.floor(Math.random () * 10 ) % 3;
+
 	if(emplCheck == is_FullTime)
 	{
-Daily_wage = Wage_per_hr * 8 ;
-System.out.println(" the daily wage of employee is " + Daily_wage );
-System.out.println("Employee is present");
+	Daily_wage = Wage_per_hr * 8 ;
+	System.out.println(" the daily wage of employee is " + Daily_wage );
+	System.out.println("Employee is present");
 
-}
+	}
+	else if( emplCheck == is_partTime)
+	{ 
+	Daily_wage = Wage_per_hr * 4;
+	System.out.println(" the daily wage of employee is " + Daily_wage );
+	System.out.println("Employee is part timmer");
+	}
 	else{
 	System.out.println("employee is absent");
+	
 }
-}
+	}
 
-}
+	}
