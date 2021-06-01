bebef4c01904f6573678bce95329152a85ce6b03
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-01 20:52:43 +0530

    refactoring the code

--- EmplWageBuilderUC1.java
+++ EmplWageBuilderUC1.java
@@ -1,60 +1,57 @@
 public class EmplWageBuilderUC1{
 	
 	
-	public static int emphrs;
-	public static int fullWorkingHour = 8;
-	public static int partTimeWorkingHour = 4;
-	public static int leave = 0;
-
-	public static int getHours(int emplCheck) {
-		switch(emplCheck) 
-		{
-			case  1:
-			
-			 emphrs = fullWorkingHour ;
-			
-			
-			break;
-
-			case 2:
-			
-			 emphrs = partTimeWorkingHour;
-			 
-			
-			break;
-
-			default:
-
-			 emphrs = leave;
-			 
-			
-		}
-		return emphrs;
+	public static final int IS_FULLTIME=1;
+	   public static final int IS_PARTTIME=2;
+	   
+	   private static int empWagePerHr;
+	   private static int Max_Working_Days;
+	   private static int Max_Working_Hrs;
+	public EmplWageBuilderUC1(int empWagePerHr, int Max_Working_Days, int Max_Working_Hrs)
+	{
+	   this.empWagePerHr = empWagePerHr;
+	   this.Max_Working_Days = Max_Working_Days;
+	   this.Max_Working_Hrs = Max_Working_Hrs;
 	}
-	
-public static void main(String args[]){
-	
 
-int Wage_per_hr=20;
-int Working_days=1;
-int totalEmpHrs = 0;
+	public void Wages()
+	{
+		int empHr=0,total_Emp_Wage=0, total_Emp_Hrs=0, totalWorkingDays=0;
+		  while(total_Emp_Hrs<=Max_Working_Hrs && totalWorkingDays<=Max_Working_Days)
+	      {
+	         totalWorkingDays++;
+	         double empCheck= Math.floor(Math.random()*10)%3;
+	         System.out.println(empCheck);
 
+	         switch ((int)empCheck)
+	         {
+	            case IS_FULLTIME:
+	               System.out.println("Employee is Present");
+	               empHr=8;
+	               break;
+	            case IS_PARTTIME:
 
-	do
-{
-	
+	               System.out.println("Parttime");
+	               empHr=4;
+	               break;
+	            default:
+	               System.out.println("Employee is Absent");
+	               empHr=0;
+	         }
+	         total_Emp_Hrs += empHr;
+	         System.out.println(totalWorkingDays);
+	         System.out.println(total_Emp_Hrs);
+	      }
+		  total_Emp_Wage=total_Emp_Hrs*empWagePerHr;
+		  System.out.println(total_Emp_Wage);
+	}
+	 public static void main(String[] args) {
 
-	int emplCheck = (int) (Math.floor(Math.random () * 10 ) % 3) ;
-	getHours(emplCheck);
-	totalEmpHrs= totalEmpHrs + emphrs;
-        System.out.println(" the total hours  is "  + totalEmpHrs );
-	Working_days++;
-}
+		 EmplWageBuilderUC1 dMart = new EmplWageBuilderUC1(20, 20, 100);
 
- while (totalEmpHrs<100 && Working_days<=25);
+	    dMart.Wages();    
+	    System.out.println(dMart);
 
-	int wage = totalEmpHrs * Wage_per_hr;
-	System.out.println(" the wage for the month is " + wage );
 	}
 
 	}
